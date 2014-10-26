35c3f90092aa092d087f9f0670aff38e7a242bbf
Stephen T Cannon <stephentcannon@gmail.com>
2014-10-26 17:57:09 -0400

    fark

--- package.js
+++ package.js
@@ -8,12 +8,14 @@ Package.describe({
   git: "https://github.com/stephentcannon/growl-alerts-manager.git"
 });
 
-Package.onUse(function (api, where) {
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
+  
   api.addFiles([
     'Alert.js'
     ],
   'client');
   
-  api.use('mizzao:bootstrap-3', 'client');
-  api.use('mrt:bootstrap-growl', 'client');
+  api.use('mizzao:bootstrap-3@3.2.0', 'client');
+  api.use('mrt:bootstrap-growl@0.1.0', 'client');
 });
