a6de01258346987ea9aa068b851fa8fdad56c28c
Stephen T Cannon <stephentcannon@gmail.com>
2014-10-26 17:47:23 -0400

    upgraded to meteor 0.9.0

--- package.js
+++ package.js
@@ -1,13 +1,19 @@
 Package.describe({
-  summary: "growl-alerts-manager"
+  summary: "growl-alerts-manager",
+  // Version number.
+  version: "0.0.5",
+  // Optional.  Default is package directory name.
+  name: "steeve:growl-alerts-manager",
+  // Optional github URL to your source repository.
+  git: "https://github.com/stephentcannon/growl-alerts-manager.git"
 });
 
-Package.on_use(function (api, where) {
-  api.add_files([
+Package.onUse(function (api, where) {
+  api.addFiles([
     'Alert.js'
     ],
   'client');
   
-  api.use('bootstrap-3', ['client']);
-  api.use('bootstrap-growl', ['client']);
+  api.use('mizzao:bootstrap-3', 'client');
+  api.use('mrt:bootstrap-growl', 'client');
 });
